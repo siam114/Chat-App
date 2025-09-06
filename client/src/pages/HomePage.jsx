86a57bf6f58a488bea221f59c7019bc62d1b7d12
siam114 <smsiamislam598@gmail.com>
2025-09-07 02:26:51 +0600

    create chat container

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='border w-full h-screen sm:px-[10%] sm:py-[5%]'>
       <h1>Home page</h1>
     </div>
   )
