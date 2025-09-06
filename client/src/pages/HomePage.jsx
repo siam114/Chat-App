e06079f342f5523cc3f0cb0ccd03f13ac0114546
siam114 <smsiamislam598@gmail.com>
2025-09-07 02:37:59 +0600

    design chat box

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSideBar from '../components/RightSideBar'
 
 const HomePage = () => {
+
+  const [selectedUser, setSelectedUser] = useState(false);
+
   return (
     <div className='border w-full h-screen sm:px-[10%] sm:py-[5%]'>
-      <div>
+      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
         <Sidebar/>
         <ChatContainer/>
         <RightSideBar/>
