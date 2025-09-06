a6710291b056ab9f0a375ec9dfae4394ea7204b6
siam114 <smsiamislam598@gmail.com>
2025-09-07 02:32:46 +0600

    import sidebar left right on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
 
 const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[10%] sm:py-[5%]'>
-      <h1>Home page</h1>
+      <div>
+        <Sidebar/>
+        <ChatContainer/>
+        <RightSideBar/>
+      </div>
     </div>
   )
 }
