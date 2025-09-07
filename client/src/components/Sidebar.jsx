f4ed0db6bd7e09a321b7c320ac0739790990ce2c
siam114 <smsiamislam598@gmail.com>
2025-09-08 01:09:58 +0600

    design searchbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
   const navigate = useNavigate()
 
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white`}>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
           <img src={assets.logo} alt="logo" className='max-w-40'/>
@@ -19,6 +19,12 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
             </div>
           </div>
         </div>
+
+        <div className='bg-[#282142] rounded-full flex items-center gap-2 px-4 py-3 mt-5'>
+          <img src={assets.search_icon} alt="search" className='w-3'/>
+          <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...' />
+        </div>
+
       </div>
     </div>
   )
