c6e92e55a7c1234203110eff0fa162774cf716cc
siam114 <smsiamislam598@gmail.com>
2025-09-08 00:09:20 +0600

    logo define on slider page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import assets from './../assets/assets';
 
-const Sidebar = () => {
+const Sidebar = ({selectedUser,setSelectedUser}) => {
   return (
     <div>
-      sidebar
+      <div className='pb-5'>
+        <div className='flex justify-between items-center'>
+          <img src={assets.logo} alt="logo" className='max-w-40'/>
+          <div className='relative py-2 group '>
+            <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
