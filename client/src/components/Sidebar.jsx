ff10b8c3ca286cc29e2e2de93422b286eb1aa677
siam114 <smsiamislam598@gmail.com>
2025-09-08 00:20:26 +0600

    design edit and logout form

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,14 +4,15 @@ import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({selectedUser,setSelectedUser}) => {
   const navigate = useNavigate()
+
   return (
-    <div>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white`}>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
           <img src={assets.logo} alt="logo" className='max-w-40'/>
           <div className='relative py-2 group '>
             <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
-            <div>
+            <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
               <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
               <hr className='my-2 border-t border-gray-500'/>
               <p className='cursor-pointer text-sm'>Logout</p>
