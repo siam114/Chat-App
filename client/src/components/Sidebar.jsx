6c0a6ea87edb9dbe6033a0651529f2cd354a5623
siam114 <smsiamislam598@gmail.com>
2025-09-08 00:14:43 +0600

    navigate profile page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import assets from './../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({selectedUser,setSelectedUser}) => {
+  const navigate = useNavigate()
   return (
     <div>
       <div className='pb-5'>
@@ -9,6 +11,11 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
           <img src={assets.logo} alt="logo" className='max-w-40'/>
           <div className='relative py-2 group '>
             <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
+            <div>
+              <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+              <hr className='my-2 border-t border-gray-500'/>
+              <p className='cursor-pointer text-sm'>Logout</p>
+            </div>
           </div>
         </div>
       </div>
