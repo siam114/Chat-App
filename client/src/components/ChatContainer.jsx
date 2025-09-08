852f7c722f77780bcd37bfa2eba3bd4eeb16b53a
siam114 <smsiamislam598@gmail.com>
2025-09-09 01:12:54 +0600

    chat box design

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { messagesDummyData } from '../assets/assets'
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
-  return (
+  return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      {/* header */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
         <p className='flex-1 text-white text-lg flex items-center gap-2'>
@@ -13,6 +14,25 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         <img onClick={()=>setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7'/>
         <img src={assets.help_icon} alt="" className='max-w-5 max-md:hidden' />
       </div>
+      {/* chat area */}
+      <div className='flex flex-col p-3 pb-6 h-[calc(100%-120)] overflow-y-scroll'>
+        {
+          messagesDummyData.map((msg,index) => (
+            <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+              {msg.image ? (
+                <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
+              ) : (
+                <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+              )}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  ) : (
+    <div className='flex flex-col justify-center items-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+      <img src={assets.logo_icon} className='max-w-16' alt="" />
+      <p className='text-lg font-medium text-white'>Chat anytime, anywhere</p>
     </div>
   )
 }
