d4b1bbb4c84252642f3555a9a7425b082ca7466a
siam114 <smsiamislam598@gmail.com>
2025-09-09 00:52:02 +0600

    cart container profile design

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const ChatContainer = () => {
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return (
-    <div>
-      chat container
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+        <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
+        <p className='flex-1 text-white text-lg flex items-center gap-2'>
+          Martin Johnson
+          <span className='h-2 w-2 rounded-full bg-green-500'></span>
+        </p>
+        <img onClick={()=>setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md:hidden max-w-7'/>
+        <img src={assets.help_icon} alt="" className='max-w-5 max-md:hidden' />
+      </div>
     </div>
   )
 }
