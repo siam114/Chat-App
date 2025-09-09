bdc107390d2c2222b39f44fb7c6f6b50c31f80f9
siam114 <smsiamislam598@gmail.com>
2025-09-10 01:20:07 +0600

    design media

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -17,7 +17,10 @@ const RightSideBar = ({selectedUser}) => {
       <hr className='my-4 border-[#ffffff50]' />
 
       <div className='px-5 text-xs'>
-        
+        <p>Media</p>
+        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+          
+        </div>
       </div>
     </div>
   )
