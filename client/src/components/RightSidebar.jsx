3b752b93ebf028079dff2402d8a0c08639996e18
siam114 <smsiamislam598@gmail.com>
2025-09-10 01:17:03 +0600

    design bio

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -4,6 +4,7 @@ import assets from '../assets/assets'
 const RightSideBar = ({selectedUser}) => {
   return selectedUser &&  (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? 'max-md:hidden' : ''}`}>
+
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} className='w-20 aspect-[1/1] rounded-full' alt="" />
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
@@ -11,6 +12,12 @@ const RightSideBar = ({selectedUser}) => {
           {selectedUser.fullName}
           </h1>
           <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+
+      <hr className='my-4 border-[#ffffff50]' />
+
+      <div className='px-5 text-xs'>
+        
       </div>
     </div>
   )
