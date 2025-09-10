098bc553c075998ff5e1af924d377cc3ed8f34fa
siam114 <smsiamislam598@gmail.com>
2025-09-11 01:54:03 +0600

    set up login page logo

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import assets from './../assets/assets';
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      {/* left */}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
     </div>
   )
 }
