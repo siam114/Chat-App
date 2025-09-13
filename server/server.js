a0212b51c571b0322497b66c92ec1c3213d1577b
siam114 <smsiamislam598@gmail.com>
2025-09-14 00:46:33 +0600

    set middlewares

--- server/server.js
+++ server/server.js
@@ -6,3 +6,9 @@ import http from 'http';
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//middlewares
+app.use(express.json({limit: "4mb"}))
+app.use(cors());
+
+app.use("/api/status", (req,res) => res.send("Server is live..."));
