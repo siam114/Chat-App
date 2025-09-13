ef03356a8d3bc74ae9f5191206547397261b168f
siam114 <smsiamislam598@gmail.com>
2025-09-13 17:13:52 +0600

    nagigate login and sign up page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -51,7 +51,7 @@ const LoginPage = () => {
           {currState === 'Sign up' ? (
             <p className='text-sm text-gray-600'>Already have an account <span onClick={()=>{setCurrState('Login'); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
           ) : (
-            <p className='text-sm text-gray-600'>Create an account <span className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
+            <p className='text-sm text-gray-600'>Create an account <span onClick={()=>setCurrState('Sign up')} className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
           )}
          </div>
 
