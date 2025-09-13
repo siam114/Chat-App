a05905a3cffa6214e075825d574ba6c0d0c0fe84
siam114 <smsiamislam598@gmail.com>
2025-09-13 20:34:36 +0600

    formating profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import assets from "../assets/assets";
 
 const ProfilePage = () => {
-
   const [selectedImg, setSelectedImg] = useState(null);
   const [name, setName] = useState("Martin Johnson");
   const [bio, setBio] = useState("Hi Everyone! I am using QuickChat.");
@@ -12,20 +11,53 @@ const ProfilePage = () => {
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex justify-center items-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex justify-between items-center max-sm:flex-col-reverse rounded-lg">
-         <form className="flex flex-col gap-5 p-10 flex-1">
+        <form className="flex flex-col gap-5 p-10 flex-1">
           <h3 className="text-lg">Profile Details</h3>
-          <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer">
-            <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden />
-            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} />
+          <label
+            htmlFor="avatar"
+            className="flex items-center gap-3 cursor-pointer"
+          >
+            <input
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImg
+                  ? URL.createObjectURL(selectedImg)
+                  : assets.avatar_icon
+              }
+              alt=""
+              className={`w-12 h-12 ${selectedImg && "rounded-full"}`}
+            />
             upload profile image
           </label>
-          <input onChange={(e)=>setName(e.target.value)} value={name} type="text" required placeholder="Your Name" className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" />
-          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder="Write profile bio" required className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" rows={4} name="" id=""></textarea>
-         </form>
-         <img src="" alt="" />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            required
+            placeholder="Your Name"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            placeholder="Write profile bio"
+            required
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            rows={4}
+            name=""
+            id=""
+          ></textarea>
+        </form>
+        <img src="" alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
