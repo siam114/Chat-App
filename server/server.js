50976483acca46566081e40164286ebd848f1f5d
siam114 <smsiamislam598@gmail.com>
2025-09-14 00:47:58 +0600

    server is live

--- server/server.js
+++ server/server.js
@@ -12,3 +12,6 @@ app.use(express.json({limit: "4mb"}))
 app.use(cors());
 
 app.use("/api/status", (req,res) => res.send("Server is live..."));
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
