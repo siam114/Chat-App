aea4d317dc4eff344ebd18a89908b0464bbff288
siam114 <smsiamislam598@gmail.com>
2025-09-15 00:56:03 +0600

    create route post method

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { login, signup } from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/signup', signup)
+userRouter.post('/login', login)
