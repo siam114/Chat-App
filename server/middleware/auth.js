7615022eb220b2f0c656432de5b8c4cc79f61b3f
siam114 <smsiamislam598@gmail.com>
2025-09-14 14:41:30 +0600

    create a middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,7 +9,11 @@ export const protectRoute =async (req, res, next) => {
 
         const user = await User.findById(decoded.userId).select("-password");
         if(!user) return res.json({success: false, message: "User not found"});
+
+        req.user = user;
+        next();
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success: false, message: error.message})
     }
 }
