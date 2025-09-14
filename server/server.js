a54cb9251af2d1614979d94ac3adb8ec9cc20d89
siam114 <smsiamislam598@gmail.com>
2025-09-15 01:58:40 +0600

    set up user route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import http from 'http';
 import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
 
 //create express app and http server
 const app = express();
@@ -12,7 +13,9 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}))
 app.use(cors());
 
+//route setup
 app.use("/api/status", (req,res) => res.send("Server is live..."));
+app.use('/api/auth', userRouter)
 
 //connect to mongodb
 await connectDB()
