fe9969dcdbb1770d805015163d6be8bb4911f1d4
siam114 <smsiamislam598@gmail.com>
2025-09-14 14:00:50 +0600

    generate error message

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,6 +32,7 @@ export const signup = async (req, res) => {
     res.json({success:true, userData:newUser, token, message: "account created successfully"})
 
    } catch (error) {
-    
+    console.log(error.message);
+    res.json({success:false, message: error.message})
    }
 }
