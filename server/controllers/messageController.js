aa19f51145f35ca599096094fffcc714960c974f
siam114 <smsiamislam598@gmail.com>
2025-09-15 02:21:48 +0600

    get all users except login user

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -19,6 +19,7 @@ export const getUsersForSidebar = async (req, res) => {
         res.json({success: true, users: filteredUsers, unseenMessages});
 
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
     }
 }
