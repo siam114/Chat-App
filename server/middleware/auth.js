9fd806a3261867dea2ccc138688905d7fc087c80
siam114 <smsiamislam598@gmail.com>
2025-09-14 14:46:36 +0600

    formating auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,19 @@
 import User from "../models/User";
 
 //middleware to protect routes
-export const protectRoute =async (req, res, next) => {
-    try {
-        const token = req.headers.token;
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await User.findById(decoded.userId).select("-password");
-        if(!user) return res.json({success: false, message: "User not found"});
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.json({ success: false, message: "User not found" });
 
-        req.user = user;
-        next();
-    } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message: error.message})
-    }
-}
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
