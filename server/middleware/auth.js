8f06b147501f778a1d207353bb6220772bea196c
siam114 <smsiamislam598@gmail.com>
2025-09-14 14:38:14 +0600

    protect route middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,15 @@
+import User from "../models/User";
+
+//middleware to protect routes
+export const protectRoute =async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user) return res.json({success: false, message: "User not found"});
+    } catch (error) {
+        
+    }
+}
