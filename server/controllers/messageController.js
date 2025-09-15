e34a61f5588b4d9c24586f1711190d0b7c3038e3
siam114 <smsiamislam598@gmail.com>
2025-09-16 01:23:28 +0600

    formatting message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -55,42 +55,41 @@ export const getMessages = async (req, res) => {
   }
 };
 
-
 //api to mark message as seen using message id
 export const markMessageAsSeen = async (req, res) => {
-    try {
-        const {id} = req.params;
-        await Message.findByIdAndUpdate(id, {seen: true});
-        res.json({success: true})
-    } catch (error) {
-         console.log(error.message);
+  try {
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
     res.json({ success: false, message: error.message });
-    }
-}
-
+  }
+};
 
 //send message to selected user
 export const sendMessage = async (req, res) => {
-    try {
-        const {text, image} = req.body;
-        const receiverId = req.params.id;
-        const senderId = req.user._id;
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
 
-        let imageUrl;
-        if(image){
-            const uploadResponse = await cloudinary.uploader.upload(image)
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        const newMessage = new Message({
-            senderId, receiverId, text, image: imageUrl
-
-        })
-
-        res.json({success: true, newMessage});
-
-    } catch (error) {
-        console.log(error.message);
-    res.json({ success: false, message: error.message });
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
     }
-}
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    res.json({ success: true, newMessage });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
