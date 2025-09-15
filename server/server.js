b8cb55f605d11de2c20f50b256172ab240ff44ec
siam114 <smsiamislam598@gmail.com>
2025-09-16 01:46:18 +0600

    socket connection handle

--- server/server.js
+++ server/server.js
@@ -25,6 +25,17 @@ export const userSocketMap = {};  // {userId: socketId}
 io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log('User connected: ', userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //emit online users to all connected users
+    io.emit('getOnlineUsers', Object.keys(userSocketMap))
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected: ', userId);
+        delete userSocketMap[userId];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap))
+    })
 })
 
 //middlewares
