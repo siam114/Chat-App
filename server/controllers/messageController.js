0e5e7fbcace4adda955fca5b296a9a4981724a63
siam114 <smsiamislam598@gmail.com>
2025-09-16 00:39:42 +0600

    formating message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -3,45 +3,53 @@ import User from "../models/User.js";
 
 //get all users except the logged in user
 export const getUsersForSidebar = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const filteredUsers = await User.find({_id: {$ne: userId}}).select("-password");
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
 
-        //count number of sms not seen
-        const unseenMessages = {}
-        const promises = filteredUsers.map(async (user) => {
-            const messages = await Message.find({senderId: user._id, receiverId: userId, seen: false});
-            if(messages.length > 0) {
-                unseenMessages[user._id] = messages.length;
-            }
-        })
-        await Promise.all(promises);
-        res.json({success: true, users: filteredUsers, unseenMessages});
-
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message });
-    }
-}
+    //count number of sms not seen
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+    await Promise.all(promises);
+    res.json({ success: true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //get all message for selected user
 export const getMessages = async (req, res) => {
-   try {
-     const {id: selectedUserId} = req.params;
-        const myId = req.user._id;
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
 
-        const messages = await Message.find({
-            $or: [
-                {senderId: myId, receiverId: selectedUserId},
-                {senderId: selectedUserId, receiverId: myId}
-            ]
-        })
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
 
-        await Message.updateMany({senderId : selectedUserId, receiverId:myId}, {seen: true});
+    await Message.updateMany(
+      { senderId: selectedUserId, receiverId: myId },
+      { seen: true }
+    );
 
-        res.json({success: true, messages});
-   } catch (error) {
+    res.json({ success: true, messages });
+  } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
-   }
-}
+  }
+};
