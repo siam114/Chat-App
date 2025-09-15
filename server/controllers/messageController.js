6f090ed789503aff7b50db9f00feafa558f3b732
siam114 <smsiamislam598@gmail.com>
2025-09-16 00:51:30 +0600

    create mark message seen function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -58,7 +58,9 @@ export const getMessages = async (req, res) => {
 //api to mark message as seen using message id
 export const markMessageAsSeen = async (req, res) => {
     try {
-        
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true});
+        res.json({success: true})
     } catch (error) {
          console.log(error.message);
     res.json({ success: false, message: error.message });
