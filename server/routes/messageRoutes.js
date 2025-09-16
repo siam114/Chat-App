0ce102be633d157cf131268e7aade002191dbefa
siam114 <smsiamislam598@gmail.com>
2025-09-17 01:15:28 +0600

    solve error

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.js';
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from '../controllers/messageController';
+import { getMessages, getUsersForSidebar, markMessageAsSeen } from '../controllers/messageController.js';
 
 const messageRouter = express.Router();
 
