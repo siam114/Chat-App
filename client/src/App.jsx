4293c3ab78a2f1a1beed55a4eeebf230ca2fbecc
siam114 <smsiamislam598@gmail.com>
2025-09-18 01:23:27 +0600

    solve error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
-import { Toster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import { AuthContext } from './context/AuthContext';
 
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain ">
-      <Toster/>
+      <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
         <Route path='/login' element={!authUser ?<LoginPage/> : <Navigate to="/"/>}/>
