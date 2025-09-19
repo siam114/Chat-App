536ec759069062afc3cc18406f002c78fde3338a
siam114 <smsiamislam598@gmail.com>
2025-09-20 02:36:58 +0600

    change dummy data

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import assets, { imagesDummyData } from "../assets/assets";
+import assets from "../assets/assets";
 import { ChatContext } from './../context/ChatContext';
 import { AuthContext } from "../context/AuthContext";
 
@@ -27,7 +27,7 @@ const RightSideBar = () => {
             alt=""
           />
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
-            <p className="w-2 h-2 rounded-full bg-gray-500"></p>
+            {onlineUsers.includes(selectedUser._id) && <p className="w-2 h-2 rounded-full bg-gray-500"></p>}
             {selectedUser.fullName}
           </h1>
           <p className="px-10 mx-auto">{selectedUser.bio}</p>
@@ -38,7 +38,7 @@ const RightSideBar = () => {
         <div className="px-5 text-xs">
           <p>Media</p>
           <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
-            {imagesDummyData.map((url, index) => (
+            {msgImages.map((url, index) => (
               <div
                 key={index}
                 onClick={() => window.open(url)}
@@ -50,7 +50,7 @@ const RightSideBar = () => {
           </div>
         </div>
 
-        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+        <button onClick={() => logout()} className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
           Logout
         </button>
       </div>
