c5f8eb51badc005f123fc3269ac9a25a156a50af
siam114 <smsiamislam598@gmail.com>
2025-09-21 03:50:08 +0600

    export server for vercel

--- server/server.js
+++ server/server.js
@@ -50,5 +50,10 @@ app.use("/api/messages", messageRouter);
 //connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+//export server for vercel
+export default server;
